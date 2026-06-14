617a360cd30d3f243b0b5b0c9724765539e0986f
Hassana <danedarakodio@gmail.com>
2026-06-14 08:35:07 +0400

    Correction variable environnement DB

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
