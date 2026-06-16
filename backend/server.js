6001b4b09c40931afc8cd63b8bb89a49b0471197
KODIO-Hassana <danedarakodio@gmail.com>
2026-06-16 14:30:09 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // 3. Importation de nos routes structurées
-const artisanRoutes = require('./routes/artisanRoutes');
-const contactRoutes = require('./routes/contactRoutes');
+const artisanRoutes = require('./Routes/artisanRoutes');
+const contactRoutes = require('./Routes/contactRoutes');
 
 // 4. Utilisation des routes
 app.use('/api/artisans', artisanRoutes);
